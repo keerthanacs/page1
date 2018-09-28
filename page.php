ee781e0324b9414919eb15fa1be78d484d54e01e
keerthanacs <43242030+keerthanacs@users.noreply.github.com>
2018-09-28 13:03:04 +0530

    Add files via upload

---
+++ page.php
@@ -0,0 +1,11 @@
+<?php
+error_reporting(E_ERROR|E_PARSE);
+$fp=fopen("counter.txt","r");
+$count=fread($fp,10);
+fclose(fp);
+$count=$count+1;
+echo"<p>page views:",$count,"</p>";
+$fp =fopen("counter.txt","w");
+fwrite($fp,$count);
+fclose($fp);
+?>
